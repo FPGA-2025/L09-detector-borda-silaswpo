1b94d35c784df48542f4b80d1e39ddbe2e76d238
silaswpo <silaswesleypereiradeoliveira@gmail.com>
2025-04-17 23:22:18 -0300

    Tentando corrigir o codigo

--- detector-borda.v
+++ detector-borda.v
@@ -1,10 +1,20 @@
 module detector_borda (
-    input clk,
-    input rst,
-    input [1:0] entrada,
-    output [1:0] detector
+    input wire clk,
+    input wire rst,
+    input wire [1:0] entrada,
+    output reg [1:0] detector
 );
-    
-//insira seu código aqui
+
+reg [1:0] entrada_antiga;
+
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        entrada_antiga <= 2'b00;
+        detector <= 2'b00;
+    end else begin
+        detector <= (~entrada_antiga) & entrada; // Detecta transição 0 → 1
+        entrada_antiga <= entrada;
+    end
+end
 
 endmodule
